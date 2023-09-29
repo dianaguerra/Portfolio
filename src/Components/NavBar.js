0df4402fca459f9d53e0303d7cbbeebf33ce25e8
Diana Guerra Elizondo <ing.dianagelizondo@gmail.com>
2023-09-29 13:04:08 -0600

    first commit

---
+++ src/Components/NavBar.js
@@ -0,0 +1,58 @@
+import { useState, useEffect } from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { HashLink } from 'react-router-hash-link';
+import {
+  BrowserRouter as Router
+} from "react-router-dom";
+
+export const NavBar = () => {
+
+  const [activeLink, setActiveLink] = useState('home');
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    }
+
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, [])
+
+  const onUpdateActiveLink = (value) => {
+    setActiveLink(value);
+  }
+
+  return (
+    <Router>
+      <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
+        <Container className="navbar-container">
+          <Navbar.Brand>
+            <h3>Portfolio.</h3>
+          </Navbar.Brand>
+         
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
+              <Nav.Link href="#about" className={activeLink === 'about' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('about')}>About</Nav.Link>
+              <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
+              <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+          <span className="navbar-text">
+              <HashLink to='#connect'>
+                <button className="vvd" onClick={ () => console.log('connect')}>          
+                  <span>Let’s Connect </span> 
+                </button>
+              </HashLink>
+            </span>
+        </Container>
+      </Navbar>
+    </Router>
+  )
+}
